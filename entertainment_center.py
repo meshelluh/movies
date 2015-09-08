088291737aaed57eb9e342c29d730121f7eec070
meshelluh <michaela@eng.fsu.edu>
2015-09-08 18:22:20 -0400

    main page done

--- entertainment_center.py
+++
@@ -1,6 +0,0 @@
-import media
-
-toy_story = media.Movie("Toy Story", 
-                        "A story of a boy and his toys that come to life, 
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_story.jpg", 
-                        "https://youtu.be/c3986gGp3Qs" )
